1cd4f8acbf99e2039e8feb4fc1c50b45b97b5386
Jon Fuller <jcfuller@sep.com>
2018-10-11 08:47:04 -0400

    add file based test for app key setting

--- Injector.Tests/InjectionTests.cs
+++ Injector.Tests/InjectionTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace Injector.Tests
@@ -6,9 +7,60 @@ namespace Injector.Tests
     public class InjectionTests
     {
         [Fact]
-        public void DoThething()
+        public void InjectsAppSettKey()
         {
-            Assert.True(true, "fail");
+            var original = @"
+<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>
+    <add key=""TestKey"" value=""original value"" />
+  </appSettings>
+</configuration>
+";
+
+            var expected = @"
+<?xml version=""1.0"" encoding=""utf-8""?>
+<configuration>
+  <appSettings>
+    <add key=""TestKey"" value=""new value"" />
+  </appSettings>
+</configuration>
+";
+
+            TestInjection(original, expected,
+                injection => injection.InjectAppSetting("TestKey", "new value"));
+        }
+
+        private void TestInjection(string originalContent, string expectedContent, Action<Injection> exercise)
+        {
+            using (WithFile(filename =>
+            {
+                var actualContent = WithContent(filename, originalContent.Trim(), () =>
+                {
+                    var injection = new Injection(filename);
+
+                    exercise(injection);
+                });
+
+
+                Assert.Equal(expectedContent.Trim(), actualContent.Trim());
+            }));
+        }
+
+        private IDisposable WithFile(Action<string> doTest)
+        {
+            var filename = Path.GetTempFileName();
+            doTest(filename);
+            return Disposing.Disposable.Create(() => File.Delete(filename));
+        }
+
+        private string WithContent(string filename, string content, Action test)
+        {
+            File.WriteAllText(filename, content);
+
+            test();
+
+            return File.ReadAllText(filename);
         }
     }
 }
